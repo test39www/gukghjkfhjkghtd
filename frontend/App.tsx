10ef20e8ee23857543fa626841ff1d6cc9a0e334
test39www <test39tes@gmail.com>
2026-07-03 20:30:39 +0300

    Add files via upload

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from "expo-status-bar"
 import { DarkTheme, NavigationContainer } from "@react-navigation/native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { RootNavigator } from "./src/navigation"
+import { AuthProvider } from "./src/context/AuthContext"
 import { theme } from "./src/theme"
 
 const navTheme = {
@@ -21,9 +22,11 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <StatusBar style="light" />
-      <NavigationContainer theme={navTheme}>
-        <RootNavigator />
-      </NavigationContainer>
+      <AuthProvider>
+        <NavigationContainer theme={navTheme}>
+          <RootNavigator />
+        </NavigationContainer>
+      </AuthProvider>
     </SafeAreaProvider>
   )
 }
